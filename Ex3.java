7899d8111688eb1770c46c1bc00d9e2ae82ab333
caiohds <caiohds2020@gmail.com>
2022-07-04 00:30:17 -0300

    Exercícios de sequência

---
+++ Ex3.java
@@ -0,0 +1,11 @@
+public class Ex3{
+	
+	public static void main (String[] args) {
+		System.out.println("1,3,5,7,9");
+		System.out.println("2,4,8,16,32,64,128");
+		System.out.println("0,1,4,9,16,25,36,49");
+		System.out.println("1,1,2,3,5,8,11");
+		System.out.println("2,10,12,16,17,18,19,200");
+	}
+}
+
